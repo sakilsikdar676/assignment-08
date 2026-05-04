63dde7009a2c03cf80ee850e6bdb5f94ebe7f53f
assignment-08 <sakilsikdar676@gmail.com>
2026-05-04 20:30:56 +0600

    final deploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
-    baseURL: "assignment-08-sage.vercel.app",
+    baseURL: "https://assignment-08-sage.vercel.app",
 })
